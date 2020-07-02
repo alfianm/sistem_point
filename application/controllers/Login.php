dcf6db8c37d8a92725bd7eca3d886db5c5333ca5
alfianm <34641846+alfianm@users.noreply.github.com>
2020-07-02 22:44:02 +0700

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -6,6 +6,10 @@ class Login extends CI_Controller {
 	 public function index()
     {
 
+        if ($this->session->userdata('username')) {
+            redirect('dashboard','refresh');
+        }
+
         $this->form_validation->set_rules('username', 'Username', 'trim|required');
         $this->form_validation->set_rules('password', 'Password', 'trim|required');
         if ($this->form_validation->run() == false ) {
